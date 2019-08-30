2026dce4bcb0408073131f0fa0151e597d92aefa
joao <jdlferreira90@gmail.com>
2019-08-30 14:31:37 +0100

    Allow CORS

--- api/main.py
+++ api/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from .routers import auth
 
 app = FastAPI(title="ovrc")
+
+# Cors, allow connections from the frontend and anywhere.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
