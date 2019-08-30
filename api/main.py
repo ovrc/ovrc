ec563ed9ca01e1873884503063f8e2d14e645d3f
joao <jdlferreira90@gmail.com>
2019-08-30 11:15:18 +0100

    Removing test users route

--- api/main.py
+++ api/main.py
@@ -1,30 +1,8 @@
-from fastapi import Depends, FastAPI, Header, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI
 
-from .routers import users, auth
+from .routers import auth
 
-app = FastAPI(
-    title="ovrc"
-)
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+app = FastAPI(title="ovrc")
 
 
-async def get_token_header(x_token: str = Header(...)):
-    if x_token != "fake-super-secret-token":
-        raise HTTPException(status_code=400, detail="X-Token header invalid")
-
-
-app.include_router(
-    users.router,
-    prefix="/users",
-    tags=["users"],
-    dependencies=[Depends(oauth2_scheme)],
-    responses={404: {"description": "Not found"}},
-)
-
-app.include_router(
-    auth.router,
-    prefix="/auth",
-    tags=["auth"]
-)
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
