34b1c555d65590223237a14bf0eeb15cdfe07801
joao <jdlferreira90@gmail.com>
2019-11-14 15:08:34 +0000

    Making use of is-active class in the sidebar

--- frontend/Sidebar.js
+++ frontend/Sidebar.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Link } from "@reach/router";
 
 const Sidebar = () => {
+  const isActive = ({ isCurrent }) => {
+    return isCurrent ? { className: "is-active" } : null;
+  };
+
   return (
     <aside className="menu">
       <p className="menu-label">General</p>
       <ul className="menu-list">
         <li>
-          <Link className="is-active" to="/">
+          <Link getProps={isActive} to="/">
             Dashboard
           </Link>
         </li>
@@ -15,7 +19,9 @@ const Sidebar = () => {
       <p className="menu-label">Administration</p>
       <ul className="menu-list">
         <li>
-          <Link to="users">Users</Link>
+          <Link getProps={isActive} to="users">
+            Users
+          </Link>
         </li>
       </ul>
     </aside>
