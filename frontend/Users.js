88cec128ac2d34e06736712dde25bb5eb1ccee23
joao <jdlferreira90@gmail.com>
2019-11-17 12:14:13 +0000

    List users + include Fontawesome

--- frontend/Users.js
+++ frontend/Users.js
@@ -7,16 +7,43 @@ const Users = () => {
   useEffect(() => {
     api_request("/users", "GET").then(res => {
       if (res.status === "success") {
-        setUsers(true);
-        console.log(res.data.users);
+        setUsers(res.data.users);
       } else {
         setUsers(false);
       }
     });
   }, []);
 
-  if (users === true) {
-    return "User list!";
+  if (users.length > 0) {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Dt. Created</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(function(user) {
+            return (
+              <tr>
+                <td>{user.username}</td>
+                <td>{user.dt_created}</td>
+                <td>
+                  <span className="icon has-text-info">
+                    <i className="fas fa-edit"></i>
+                  </span>
+                  <span className="icon has-text-danger">
+                    <i className="fas fa-ban"></i>
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
   }
 
   return "Loading...";
